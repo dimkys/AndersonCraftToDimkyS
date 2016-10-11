795e5d672b3b0f5004db32e41c57ddbf5acf650c
dimkys <x-18.ru@mail.ru>
2016-10-11 23:56:08 +0300

    д

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function accept(req, res){
 }
 console.log("Dimkys Start, 5 minuts fly norm");
 if (!module.parent) {
-    http.createServer(accept).listen(1337);
+    http.createServer(accept).listen(8080);
 } else {
     exports.accept = accept;
 }
