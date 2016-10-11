4c7fcac4ea73c4256f988e85a371e59df391fe6d
dimkys <x-18.ru@mail.ru>
2016-10-11 15:34:38 +0300

    Удаление предметов при перетаскивании на кнопку

--- javascript/event.js
+++ javascript/event.js
@@ -2,7 +2,8 @@ resources.ondrop=drop;
 resources.ondragover=allowDrop;
 worktop.ondrop=drop;
 worktop.ondragover=allowDrop;
-
+buttonCraft.ondragover=allowDrop;
+buttonCraft.ondrop=drop;
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 
@@ -10,9 +11,15 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
+    var elem=document.getElementById(data);
     if(ev.target.attributes.id.value==='resources'||ev.target.attributes.id.value==='worktop')
     {
-        ev.target.appendChild(document.getElementById(data));
+        ev.target.appendChild(elem);
+    }else{
+        if(ev.target.attributes.id.value==='buttonCraft'&& elem.id>5){
+            ev.target.appendChild(elem);
+            removeChildren(ev.target)    ;
+        }
     }
 }
 function allowDrop(ev) {
